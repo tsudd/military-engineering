2c0a87570710c41e7ad6864da06119c9e17df71c
fedjaz <fedjaz5@gmail.com>
2021-08-25 21:36:45 +0300

    Created Initial Kisel project

---
+++ Military Engineering/CalculationsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculationsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculationsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f538e760-4b96-413e-92ae-963cf328d40f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Military Engineering/CalculationsTest/UnitTestKisel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculationsTest
+{
+    [TestClass]
+    public class UnitTestKisel
+    {
+        [TestMethod]
+        public void TestKisel()
+        {
+            Assert.AreEqual(CalculationsCore.Initial.Hello, "Hello Kisel");
+        }
+    }
+}
