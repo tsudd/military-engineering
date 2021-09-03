c7a919292913820167709e2530803ca4d27b0d56
fedjaz <fedjaz5@gmail.com>
2021-09-03 22:42:12 +0300

    oops

--- Military Engineering/Military Engineering/Fortification/BuildingElementPanel.Designer.cs
+++ Military Engineering/Military Engineering/Fortification/BuildingElementPanel.Designer.cs
@@ -5,10 +5,11 @@ namespace Military_Engineering.Fortification
     {
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        public System.Windows.Forms.Label label3;
-        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label SecondTurnLabel;
+        public System.Windows.Forms.Label FutureTurnLabel;
         public System.Windows.Forms.Label label1;
-        public System.Windows.Forms.Label InfoLabel;
+        public System.Windows.Forms.Label FirstTurnLabel;
+        public System.Windows.Forms.Label AllTurnsLabel;
 
         /// <summary> 
         /// Required designer variable.
@@ -38,10 +39,11 @@ namespace Military_Engineering.Fortification
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.InfoLabel = new System.Windows.Forms.Label();
+            this.FirstTurnLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.FutureTurnLabel = new System.Windows.Forms.Label();
+            this.SecondTurnLabel = new System.Windows.Forms.Label();
+            this.AllTurnsLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
@@ -59,42 +61,44 @@ namespace Military_Engineering.Fortification
             this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 10);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.71429F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.28571F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1180, 330);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.18391F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.81609F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1026, 348);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 4;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.SecondTurnLabel, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.FutureTurnLabel, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.InfoLabel, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.FirstTurnLabel, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.AllTurnsLabel, 4, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1174, 161);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1020, 193);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // InfoLabel
+            // FirstTurnLabel
             // 
-            this.InfoLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.InfoLabel.Font = new System.Drawing.Font("Bahnschrift", 15F);
-            this.InfoLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
-            this.InfoLabel.Location = new System.Drawing.Point(296, 0);
-            this.InfoLabel.Name = "InfoLabel";
-            this.InfoLabel.Size = new System.Drawing.Size(287, 80);
-            this.InfoLabel.TabIndex = 4;
-            this.InfoLabel.Text = "Name";
-            this.InfoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.FirstTurnLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FirstTurnLabel.Font = new System.Drawing.Font("Bahnschrift", 15F);
+            this.FirstTurnLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
+            this.FirstTurnLabel.Location = new System.Drawing.Point(207, 0);
+            this.FirstTurnLabel.Name = "FirstTurnLabel";
+            this.FirstTurnLabel.Size = new System.Drawing.Size(198, 96);
+            this.FirstTurnLabel.TabIndex = 4;
+            this.FirstTurnLabel.Text = "FirstTurnLabel";
+            this.FirstTurnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label1
             // 
@@ -103,34 +107,46 @@ namespace Military_Engineering.Fortification
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(287, 80);
+            this.label1.Size = new System.Drawing.Size(198, 96);
             this.label1.TabIndex = 5;
             this.label1.Text = "Name";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label2
+            // FutureTurnLabel
             // 
-            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Font = new System.Drawing.Font("Bahnschrift", 15F);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
-            this.label2.Location = new System.Drawing.Point(882, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(289, 80);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Name";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.FutureTurnLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FutureTurnLabel.Font = new System.Drawing.Font("Bahnschrift", 15F);
+            this.FutureTurnLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
+            this.FutureTurnLabel.Location = new System.Drawing.Point(615, 0);
+            this.FutureTurnLabel.Name = "FutureTurnLabel";
+            this.FutureTurnLabel.Size = new System.Drawing.Size(198, 96);
+            this.FutureTurnLabel.TabIndex = 6;
+            this.FutureTurnLabel.Text = "FutureTurnLabel";
+            this.FutureTurnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label3
+            // SecondTurnLabel
             // 
-            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Font = new System.Drawing.Font("Bahnschrift", 15F);
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
-            this.label3.Location = new System.Drawing.Point(589, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(287, 80);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Name";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SecondTurnLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SecondTurnLabel.Font = new System.Drawing.Font("Bahnschrift", 15F);
+            this.SecondTurnLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
+            this.SecondTurnLabel.Location = new System.Drawing.Point(411, 0);
+            this.SecondTurnLabel.Name = "SecondTurnLabel";
+            this.SecondTurnLabel.Size = new System.Drawing.Size(198, 96);
+            this.SecondTurnLabel.TabIndex = 7;
+            this.SecondTurnLabel.Text = "SecondTurnLabel";
+            this.SecondTurnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // AllTurnsLabel
+            // 
+            this.AllTurnsLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AllTurnsLabel.Font = new System.Drawing.Font("Bahnschrift", 15F);
+            this.AllTurnsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
+            this.AllTurnsLabel.Location = new System.Drawing.Point(819, 0);
+            this.AllTurnsLabel.Name = "AllTurnsLabel";
+            this.AllTurnsLabel.Size = new System.Drawing.Size(198, 96);
+            this.AllTurnsLabel.TabIndex = 8;
+            this.AllTurnsLabel.Text = "AllTurnsLabel";
+            this.AllTurnsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // BuildingElementPanel
             // 
@@ -139,7 +155,7 @@ namespace Military_Engineering.Fortification
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(27)))), ((int)(((byte)(42)))));
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "BuildingElementPanel";
-            this.Size = new System.Drawing.Size(1200, 353);
+            this.Size = new System.Drawing.Size(1046, 371);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
